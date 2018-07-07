244fbfb392c3f771f258cfeebc10e38b09e8331c
Mario Lopriore <mariojames211@gmail.com>
2018-07-07 16:10:47 -0700

    Another round of edits and browser testing...

--- includes/portfolio.php
+++ includes/portfolio.php
@@ -10,28 +10,28 @@
         <img data-src="images/beulah.jpg" alt="Beulahland PDX">
         <div class="overlay">
             <h2 class="h-on-hover">Beulahland</h2>
-            <p class="p-on-hover">Beulahland is a unique family-owned, old-school Portland restaurant and lounge. This responsive WordPress site features blog categories for publishing fresh content and a built-in social media sidabar.</p>   
+            <p class="p-on-hover">Beulahland is a unique family-owned, old-school Portland restaurant and lounge. This responsive WordPress site features blog categories for publishing fresh content and a built-in social media sidebar.</p>   
         </div>
     </a>
     <a href="http://www.pccbridge.com" target="_blank" rel="nofollow" class="overlay-text">
         <img data-src="images/bridge.jpg" alt="PCC's student newspaper The Bridge">
         <div class="overlay">
             <h2 class="h-on-hover">PCC's The Bridge</h2>
-            <p class="p-on-hover">The Bridge is the student newspaper of Portland Community College. The new site is ready for prime time. Legacy documentation and a child theme provide future stability as new editors work on the project.</p>   
+            <p class="p-on-hover">The Bridge is the student newspaper of Portland Community College. The new WordPress site is constantly updated. Legacy documentation and a child theme provide future stability as new editors work on the project.<span class="sign-off">-30-</span></p>   
         </div>
     </a>
     <a href="http://www.anchortagdesign.com/boots4" target="_blank" rel="nofollow" class="overlay-text">
         <img data-src="images/boots-thumb.jpg" alt="bootstrap 4 template logo">
         <div class="overlay">
             <h2 class="h-on-hover">Bootstrap 4</h2>
-            <p class="p-on-hover">Bootstrap is a popular and FREE front-end framework for web development. It features HTML &amp; CSS templates for forms, buttons, navigation systems other elements for building responsive web design.</p>   
+            <p class="p-on-hover">This sample site is built with Bootstrap, a popular and versitile front-end framework for web development. It features HTML &amp; CSS templates for forms, buttons, navigation systems other elements for building responsive web design.</p>   
         </div>
     </a>
     <a href="https://www.worldwidewario.com/cms" target="_blank" rel="nofollow" class="overlay-text">
         <img data-src="images/maynard.jpg" alt="the range finder">
         <div class="overlay">
             <h2 class="h-on-hover">Maynard's site</h2> 
-            <p class="p-on-hover">This is Craig Maynard Scott's photography portfolio site. Currently it's just sample images &amp; placeholder text. Live site coming in early 2018. Features: mobile first design and social media links.</p>   
+            <p class="p-on-hover">This is Craig Maynard Scott's online photography portfolio. Currently, it's just sample images &amp; placeholder text. Live site coming mid 2018. Features: mobile first design and social media links.</p>   
         </div>
     </a>
     <a href="https://www.anchortagdesign.com/ace" target="_blank" rel="nofollow" class="overlay-text">
