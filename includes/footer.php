c36db5dbd7ac33017e4f94925f90aea4dab7758f
Mario Lopriore <mariojames211@gmail.com>
2018-09-14 15:08:18 -0700

    Continuing to go down the Let's Validate checklist...

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,11 @@
 <footer class="footer" id="footer">
+
 	<div class="footer-grid">
-		<section class="address">
-			<p class="tel">(5&#48;3) 351-364Zero</p>
-			<p>P.O. Box 11483
-				<br> Portland, OR 97211</p>
-		</section>
+		
 		<div class="icon">
 			<img data-src="images/atd-logo-links.png" alt="anchor tag design logo">
 		</div>
+
 		<section class="social">
 			<h4>
 				<script type="text/javascript">
@@ -29,9 +27,14 @@
 				<a href="https://www.instagram.com/anchor_tag_design/" target="_blank" rel="nofollow noopener" title="instagram feed"><i class="fab fa-instagram"></i></a>
 				<a href="https://www.facebook.com/anchortagdesign/" target="_blank" rel="nofollow noopener" title="facebook page"><i class="fab fa-facebook-square"></i></a>
 				<a data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="mario-lopriore" class="LI-simple-link" href="https://www.linkedin.com/in/mario-lopriore?trk=profile-badge" target="_blank" rel="nofollow noopener" title="linkedin link"><i class="fab fa-linkedin"></i></a>
-				
 			</h4>
-			<p><span class="copyright">&copy;</span> 2018 Mario LoPriore</p>
+		</section>
+		<section class="address">
+			<!-- <p class="tel">(5&#48;3) 351-364Zero</p> -->
+			<p>P.O. Box 11483
+				<br> Portland, OR 97211</p>
+			<p><span class="copyright">&copy;</span>&nbsp;<?php echo date("Y"); ?></p>
 		</section>
 	</div>
+
 </footer>
--- index.php
+++ index.php
@@ -48,6 +48,7 @@
 		],
 		"@id": "https://www.anchortagdesign.com/",
 		"name": "Anchor Tag Design",
+		"short_name": "Anchor Tag",
 		"address": {
 			"@type": "PostalAddress",
 			"postOfficeBoxNumber": "11483",
@@ -103,17 +104,6 @@
 	
 </head>
 <body>
-
-	<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=2074499839536564&autoLogAppEvents=1';
-			fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-		</script>
-
 	<?php include 'includes/navigation.php'; ?>
 	<?php include 'includes/header.php'; ?>
 	<?php include 'includes/portfolio.php'; ?>
