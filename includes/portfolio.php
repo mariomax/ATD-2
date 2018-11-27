122e1c5271ee2bc878f710be07fb68e465368461
Mario Lopriore <mariojames211@gmail.com>
2018-11-26 20:39:23 -0800

    Fixed PHPMailer again, but using a gitignore. Added new portfolio cards.

--- includes/portfolio.php
+++ includes/portfolio.php
@@ -26,19 +26,26 @@
 			<p class="p-on-hover">Wedgehead is the premier pinball arcade in Portland, Oregon. Serving bad-ass bar food, stiff drinks, and cold beer. Home to 25 rotating pinball machines from all eras. Their site is a fast, mobile first web build.</p>   
 		</div>
 	</a>
+	<a href="https://www.kitchentablemagazine.com" target="_blank" rel="nofollow noopener" class="overlay-text">
+		<img src="images/ktm-card.jpg" alt="Kitchen Table Magazine Kickstarter">
+		<div class="overlay">
+			<h2 class="h-on-hover">Kitchen Table Magazine</h2>
+			<p class="p-on-hover">A new print and digital publication connecting adventurous souls, curious cooks, and enthusiastic eaters with talented writers, artists, cartoonists, and photographers who explore not only the how-to’s of cooking, but the why’s of eating. A WordPress blog &amp; shop.</p>   
+		</div>
+	</a>
+	<a href="https://www.cascadiapiper.com" target="_blank" rel="nofollow noopener" class="overlay-text">
+		<img src="images/piper.jpg" alt="John Goff: the Cascadia Piper">
+		<div class="overlay">
+			<h2 class="h-on-hover">Cascadia Piper</h2>
+			<p class="p-on-hover"><em>John Goff: The Cascadia Piper</em><br />A mobile first site was built from scratch with Twitter, Facebook and Schema markup to maximize SEO. A Facebook page feed, audio players, and an image gallery provide context. Contact info is above the fold for easy access.</p>   
+		</div>
+	</a>
 	<a href="https://www.beulahlandpdx.com" target="_blank" rel="nofollow noopener" class="overlay-text">
 		<img src="images/new-beulah.jpg" alt="Beulahland PDX">
 		<div class="overlay">
 			<h2 class="h-on-hover">Beulahland</h2>
 			<p class="p-on-hover">Beulahland is a unique, family-owned, old-school Portland restaurant and lounge. This responsive WordPress site features blog categories for publishing fresh content and a built-in social media sidebar.</p>   
 		</div>
-	</a>
-	<a href="https://www.cascadiapiper.com" target="_blank" rel="nofollow noopener" class="overlay-text">
-	<img src="images/piper.jpg" alt="John Goff: the Cascadia Piper">
-	<div class="overlay">
-		<h2 class="h-on-hover">Cascadia Piper</h2>
-		<p class="p-on-hover"><em>John Goff: The Cascadia Piper</em><br />A mobile first site was built from scratch with Twitter, Facebook and Schema markup to maximize SEO. A Facebook page feed, audio players, and an image gallery provide context. Contact info is above the fold for easy access.</p>   
-	</div>
 	</a>
 	<a href="https://www.worldwidewario.com/cms" target="_blank" rel="nofollow noopener" class="overlay-text">
 		<img src="images/maynard.jpg" alt="the range finder">
@@ -52,13 +59,6 @@
 		<div class="overlay">
 			<h2 class="h-on-hover">Bootstrap 4</h2>
 			<p class="p-on-hover">This sample site is built with Bootstrap 4, a popular and versitile framework for web development. It features HTML &amp; CSS templates for forms, buttons, navigation systems other elements for building responsive web design.</p>   
-		</div>
-	</a>
-	<a href="https://www.anchortagdesign.com/ace" target="_blank" rel="nofollow noopener" class="overlay-text">
-		<img src="images/ace-thumb.jpg" alt="Ace in the Hole MultiSports">
-		<div class="overlay">
-			<h2 class="h-on-hover">Ace in the Hole</h2>
-			<p class="p-on-hover"><em>"MultiSports for Every Body."</em><br />The Pacific Northwest's most inclusive and fun weekend event. Features: PHP registration form, Facebook feed, Twitter feed, image gallery, and live weather report widget.</p>   
 		</div>
 	</a>
 </section>
