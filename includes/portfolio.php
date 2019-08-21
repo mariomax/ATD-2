715016422a65771580fa7cd21c4a6fb7453faeb6
Mario Lopriore <mariojames211@gmail.com>
2019-08-21 09:56:55 -0700

    Fixed a typo... in the intro to the site!

--- includes/portfolio.php
+++ includes/portfolio.php
@@ -1,6 +1,6 @@
 <section class="intro">
 	<h2>Web Design:</h2>
-	<h3>Sites dsigned and built for artists, makers, small businesses, and the service industry</h3>
+	<h3>Sites designed and built for artists, makers, small businesses, and the service industry</h3>
 	<div class="intro-wrapper">
 		<div class="intro-para">
 			<p>For years I studied and worked in “applied art” - photography, graphic and print design, screen printing, etc. This became the foundation for my transition into web design. The catalyst was the desire to “digitize” my skills for the modern workplace. I've accumulated decades of experience in the print and service industries in Portland and leveraging this accumulated know-how is the value added benefit I bring to my clients.<br/>&#35;ilovewhatido</p>
@@ -19,6 +19,7 @@
 		</div>
 	</div>
 </section>
+<!-- Portfolio cards. -->
 <section class="thumbs" id="portfolio">
 	<a href="https://wedgeheadpdx.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Wedgehead PDX">
 		<img src="images/wedgehead-cap.jpg" alt="Wedgehead pinball arcade Portland">
@@ -48,11 +49,11 @@
 			<p class="p-on-hover">DMG North is a manufacturers’ representative for custom engineered, high quality Heating, Ventilating and A/C (HVAC) equipment. This responsive site features training session registration and multiple contact options including built-in text used to connect with tech support.</p>   
 		</div>
 	</a>
-	<a href="https://saucyjewelry.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Saucy Jewelry">
-		<img src="images/saucy-card.jpg" alt="Saucy Jewelry | Alpine, Oregon">
+	<a href="https://sanghata.net" target="_blank" rel="nofollow noopener" class="overlay-text" title="Sanghata Foundation photo">
+		<img src="images/sanghata-card.jpg" alt="Sanghata Foundation info card">
 		<div class="overlay">
-			<h2 class="h-on-hover">Saucy Jewelry</h2>
-			<p class="p-on-hover">The styling on Elisa's site allows the jewelry to shine - literally. The minimal color scheme comes from the natural backdrops in her photos. The eCommerce shop features over 100 handmade items: rings, pendants, earrings and bracelets. Most of these have variations - finish, size, gemstone selection, etc. The visitor picks their piece...</p>   
+			<h2 class="h-on-hover">Sanghata</h2>
+			<p class="p-on-hover">The non-profit Sanghata Foundation manages monetary donations given to the Theravada Buddhist monks of the Pacific Hermitage in White Salmon, WA. The site has a DonorBox feature to accept alms in an online platform and a blog for the monks to post news items.</p>
 		</div>
 	</a>
 	<a href="https://leathaconsulting.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Leatha Consulting LLC">
@@ -60,6 +61,13 @@
 		<div class="overlay">
 			<h2 class="h-on-hover">Leatha Consulting LLC</h2> 
 			<p class="p-on-hover">This company specializes in professional eDiscovery Consulting and Digital Forensics. They wanted a site that's clean, fast and to-the-point. WordPress provides that and makes it easy to add more pages / content as they grow.</p>   
+		</div>
+	</a>
+	<a href="https://saucyjewelry.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Saucy Jewelry">
+		<img src="images/saucy-card.jpg" alt="Saucy Jewelry | Alpine, Oregon">
+		<div class="overlay">
+			<h2 class="h-on-hover">Saucy Jewelry</h2>
+			<p class="p-on-hover">The styling on Elisa's site allows the jewelry to shine - literally. The minimal color scheme comes from the natural backdrops in her photos. The eCommerce shop features over 100 handmade items: rings, pendants, earrings and bracelets. Most of these have variations - finish, size, gemstone selection, etc. The visitor picks their piece...</p>   
 		</div>
 	</a>
 	<a href="https://www.kitchentablemagazine.com" target="_blank" rel="nofollow noopener" class="overlay-text" title="Kitchen Table Magazine">
@@ -83,13 +91,13 @@
 			<p class="p-on-hover"><em>The Bridge:</em> the student newspaper of Portland Community College (PCC). The WordPress site is updated each term. Legacy documentation and a child theme provide future stability as new editors work on the project. #givingback<span class="sign-off">-30-</span></p>   
 		</div>
 	</a> -->
-	<a href="" target="_blank" rel="nofollow noopener" class="overlay-text" title="Helper Honey Logo">
+	<!-- <a href="" target="_blank" rel="nofollow noopener" class="overlay-text" title="Helper Honey Logo">
 		<img src="images/beestie-circle.png" alt="Helper Honey Logo">
 		<div class="overlay">
 			<h2 class="h-on-hover">Helper Honey CBD</h2>
 			<p class="p-on-hover">Raw, full spectrum CBD grown in Oregon's beautiful Willamette Valley. "White label" CBD honey sticks, beeswax &amp; soy candles, and beeswax natural food wraps. Here's the logo - site to follow soon...</p>
 		</div>
-	</a>
+	</a> -->
 	<a href="https://www.anchortagdesign.com/contact/" class="overlay-text" title="Your Project">
 		<img src="images/your-project.jpg" alt="Google audit on Anchor Tag Design">
 		<div class="overlay">
