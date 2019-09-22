1f7732714d48a035823704c0349ef88818f0c60a
Mario Lopriore <mariojames211@gmail.com>
2019-09-21 17:32:26 -0700

    New cards in the portfolio.

--- includes/portfolio.php
+++ includes/portfolio.php
@@ -35,11 +35,11 @@
 			<p class="p-on-hover">Serving SE Portland since 2001, The Goodfoot is a neighborhood staple. Live music &amp; DJs, great food, full bar, pool &amp; pinball, and daily happy hour. The site features months of music listings, the food menu, and art show updates - all in a mobile friendly format.</p>   
 		</div>
 	</a>
-	<a href="http://www.beulahlandpdx.com" target="_blank" rel="nofollow noopener" class="overlay-text" title="Beulahland">
-		<img src="images/new-beulah.jpg" alt="Beulahland PDX">
+	<a href="https://www.vivid-element.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Vivid Element's clothing line">
+		<img src="images/vivid-element-card.jpg" alt="Vivid Element">
 		<div class="overlay">
-			<h2 class="h-on-hover">Beulahland</h2>
-			<p class="p-on-hover">Beulahland is a unique, family-owned, old-school Portland restaurant and lounge. This responsive WordPress site features blog categories for publishing fresh content and a built-in social media sidebar.</p>   
+			<h2 class="h-on-hover">Vivid Element</h2>
+			<p class="p-on-hover">the student newspaper of Portland Community College (PCC). The WordPress site is updated each term. Legacy documentation and a child theme provide future stability as new editors work on the project.</p>   
 		</div>
 	</a>
 	<a href="https://www.dmgn.com" target="_blank" rel="nofollow noopener" class="overlay-text" title="DMG North: Innovative HVAC Solutions">
@@ -56,6 +56,13 @@
 			<p class="p-on-hover">The non-profit Sanghata Foundation manages monetary donations given to the Theravada Buddhist monks of the Pacific Hermitage in White Salmon, WA. The site has a DonorBox feature to accept alms in an online platform and a blog for the monks to post news items.</p>
 		</div>
 	</a>
+	<a href="https://saucyjewelry.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Saucy Jewelry">
+		<img src="images/saucy-card.jpg" alt="Saucy Jewelry | Alpine, Oregon">
+		<div class="overlay">
+			<h2 class="h-on-hover">Saucy Jewelry</h2>
+			<p class="p-on-hover">The styling on Elisa's site allows the jewelry to shine - literally. The minimal color scheme comes from the natural backdrops in her photos. The eCommerce shop features over 100 handmade items: rings, pendants, earrings and bracelets. Most of these have variations - finish, size, gemstone selection, etc. The visitor picks their piece...</p>   
+		</div>
+	</a>
 	<a href="https://leathaconsulting.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Leatha Consulting LLC">
 		<img src="images/leatha-consulting.jpg" alt="Leatha Consulting LLC | Digital Forensics">
 		<div class="overlay">
@@ -63,11 +70,11 @@
 			<p class="p-on-hover">This company specializes in professional eDiscovery Consulting and Digital Forensics. They wanted a site that's clean, fast and to-the-point. WordPress provides that and makes it easy to add more pages / content as they grow.</p>   
 		</div>
 	</a>
-	<a href="https://saucyjewelry.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Saucy Jewelry">
-		<img src="images/saucy-card.jpg" alt="Saucy Jewelry | Alpine, Oregon">
+	<a href="http://www.beulahlandpdx.com" target="_blank" rel="nofollow noopener" class="overlay-text" title="Beulahland">
+		<img src="images/new-beulah.jpg" alt="Beulahland PDX">
 		<div class="overlay">
-			<h2 class="h-on-hover">Saucy Jewelry</h2>
-			<p class="p-on-hover">The styling on Elisa's site allows the jewelry to shine - literally. The minimal color scheme comes from the natural backdrops in her photos. The eCommerce shop features over 100 handmade items: rings, pendants, earrings and bracelets. Most of these have variations - finish, size, gemstone selection, etc. The visitor picks their piece...</p>   
+			<h2 class="h-on-hover">Beulahland</h2>
+			<p class="p-on-hover">Beulahland is a unique, family-owned, old-school Portland restaurant and lounge. This responsive WordPress site features blog categories for publishing fresh content and a built-in social media sidebar.</p>   
 		</div>
 	</a>
 	<a href="https://www.kitchentablemagazine.com" target="_blank" rel="nofollow noopener" class="overlay-text" title="Kitchen Table Magazine">
@@ -77,27 +84,20 @@
 			<p class="p-on-hover">A new print and digital publication connecting adventurous souls, curious cooks, and enthusiastic eaters with talented writers, artists, cartoonists, and photographers who explore not only the how-to’s of cooking, but the why’s of eating. A WordPress blog &amp; shop.</p>   
 		</div>
 	</a>
-	<!-- <a href="https://www.ashleyowenphoto.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Ashley Owen Photo">
+	<a href="https://www.ashleyowenphoto.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="Ashley Owen Photo">
 		<img src="images/ashley.jpg" alt="Ashley Owen Photo">
 		<div class="overlay">
 			<h2 class="h-on-hover">Ashley Owen Photo</h2>
 			<p class="p-on-hover"><em>Ashley's</em> site is a completely bare-bones WordPress build with custom post templates designed to make adding more galleries a snap. Features: responsive design, minimal &amp; clean lightbox styling, and social media links.</p>   
 		</div>
-	</a> -->
-	<!-- <a href="https://www.pccbridge.com/" target="_blank" rel="nofollow noopener" class="overlay-text" title="The Bridge: PCC's Student Newspaper">
-		<img src="images/bridge.jpg" alt="Ashley Owen Photo">
-		<div class="overlay">
-			<h2 class="h-on-hover">PCC's The Bridge</h2>
-			<p class="p-on-hover"><em>The Bridge:</em> the student newspaper of Portland Community College (PCC). The WordPress site is updated each term. Legacy documentation and a child theme provide future stability as new editors work on the project. #givingback<span class="sign-off">-30-</span></p>   
-		</div>
-	</a> -->
-	<!-- <a href="" target="_blank" rel="nofollow noopener" class="overlay-text" title="Helper Honey Logo">
+	</a>
+	<a href="" target="_blank" rel="nofollow noopener" class="overlay-text" title="Helper Honey Logo">
 		<img src="images/beestie-circle.png" alt="Helper Honey Logo">
 		<div class="overlay">
 			<h2 class="h-on-hover">Helper Honey CBD</h2>
 			<p class="p-on-hover">Raw, full spectrum CBD grown in Oregon's beautiful Willamette Valley. "White label" CBD honey sticks, beeswax &amp; soy candles, and beeswax natural food wraps. Here's the logo - site to follow soon...</p>
 		</div>
-	</a> -->
+	</a>
 	<a href="https://www.anchortagdesign.com/contact/" class="overlay-text" title="Your Project">
 		<img src="images/your-project.jpg" alt="Google audit on Anchor Tag Design">
 		<div class="overlay">
